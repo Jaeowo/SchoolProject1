f1f4f27630d2c3033bbd393607a2ac5c17af696a
Jaeowo <117854534+Jaeowo@users.noreply.github.com>
2025-06-29 15:25:04 +0900

    BoatMoving

---
+++ tgs/Assets/Script/RiverScene/BoatMoving.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class BoatMoving : MonoBehaviour
+{
+    private float[] lanes = new float[] { 2.9f, 5.8f, 8.7f };
+    private int currentLane = 1;
+
+    // Floating Effect
+    private float frequency = 1.5f;
+    private float floatRange = 0.5f;
+    private Vector3 basePosition;
+
+    private void Start()
+    {
+        basePosition = transform.position;
+        basePosition.y = lanes[currentLane];
+    }
+
+    void Update()
+    {
+        HandleLaneInput();
+        ApplyFloating();
+    }
+
+    void HandleLaneInput()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.JoystickButton3))
+        {
+            if (currentLane > 0)
+            {
+                currentLane--;
+                basePosition.y = lanes[currentLane];
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.JoystickButton4))
+        {
+            if (currentLane < lanes.Length - 1)
+            {
+                currentLane++;
+                basePosition.y = lanes[currentLane];
+            }
+        }
+    }
+
+    void ApplyFloating()
+    {
+        float yOffset = Mathf.PerlinNoise(Time.time * 0.5f, 0f) * floatRange;
+        float zRot = Mathf.Sin(Time.time * (frequency * 0.8f)) * 2f;
+
+        transform.position = basePosition + Vector3.up * yOffset;
+        transform.rotation = Quaternion.Euler(0, 0, zRot);
+    }
+}
